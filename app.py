cf6e2798a9d1a07f67eb473c2dfac34d250143f1
guiOsorio <gosorio@sandiego.edu>
2021-07-27 15:35:42 +0100

    Validation file implemented

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from wtforms.fields.html5 import DateField
 from wtforms import StringField, SelectField
 from datetime import datetime
 
+from validation import validate
+
 # Initialize app
 app = Flask(__name__)
 
@@ -45,10 +47,10 @@ possibletimesweekend = ["Choose a time",
     "19:00"
 ]
 
-# Form fields
+# Reservation Form
 class BookingForm(FlaskForm):
     name = StringField('Name')
-    people = SelectField('People', choices=numofpeople)
+    people = SelectField('Number of people', choices=numofpeople)
     court = SelectField('Court', choices=courts)
     date = DateField('Date', format='%Y-%m-%d', default=datetime.now())
     time = SelectField('Time (open from 09:00 to 22:00)', choices=possibletimes, default="Choose a time")
@@ -73,62 +75,21 @@ def form():
         date = form.date.data # datetime.date
         time = form.time.data # string
 
-        if name == "" or people == "" or court == "" or date == "" or time == "Choose a time":
-            flash("Please fill out all fields")
-            return render_template("form.html", form=form)
-
-        selected_date_str = date.strftime("%Y-%m-%d") # string selected date 
-        current_time_str = datetime.now().strftime("%H:%M") # string
-        selected_weekday = date.strftime("%a") # string current day of the week abbreviation (3 characters)
-
-        # selected date/time data in int
-        selected_hour = int(time[:2])
-        selected_minute = int(time[3:])
-        selected_year = int(selected_date_str[:4])
-        selected_month = int(selected_date_str[5:7])
-        selected_day = int(selected_date_str[8:])
-
-        # current date/time data in int
-        current_hour = int(current_time_str[:2]) # int
-        current_minute = int(current_time_str[3:]) # int
-        current_year = datetime.now().year # int
-        current_month = datetime.now().month # int
-        current_day = datetime.now().day # int
-
-        # Own validation
-        if people not in numofpeople:
-            flash("Only 2 to 4 people are allowed per court")
-            return render_template("form.html", form=form)
-        elif court not in courts:
-            flash("Please select a valid court")
-            return render_template("form.html", form=form)
-        elif ((selected_weekday == "Sat" or selected_weekday == "Sun") and time not in possibletimesweekend) or time not in possibletimes:
-            flash("The club is not open at this time. Week hours are 09:00 to 22:00. Weekend hours are 09:00 to 20:00 (reservations are allowed until 1 hour before closing)")
-            return render_template("form.html", form=form)
-        elif selected_year < current_year or (selected_year == current_year and (selected_month < current_month) or (selected_month == current_month and selected_day < current_day)):
-            flash("Please provide a valid date")
-            return render_template("form.html", form=form)
-        elif selected_year == current_year and selected_month == current_month and selected_day == current_day and selected_hour <= current_hour + 1:
-            if selected_hour == current_hour + 1 and (60 + selected_minute - current_minute) <= 30:
-                flash("Online reservations can only be made at least 30 minutes prior to palying time")
-                return render_template("form.html", form=form)
-            elif selected_hour == current_hour + 1 and (60 + selected_minute - current_minute) > 30:
-                return render_template("data.html", name=name, people=people, court=court, date=date, time=time)
-            else:
-                flash("Please select a valid time")
-                return render_template("form.html", form=form)
-        else:
+        if validate(name, people, court, date, time, numofpeople, courts, possibletimesweekend, possibletimes):
             return render_template("data.html", name=name, people=people, court=court, date=date, time=time)
+            
 
     return render_template("form.html", form=form)
 
 
 # REQUIREMENTS
+    # implement login
     # form 
-        # displays a calendar daypicker but withinvalid dates disabled (already past dates, days where club is closed, days more than a month from today)
+        # displays a calendar daypicker but with invalid dates disabled (already past dates, days where club is closed, days more than a month from today)
         # displays only options of valid times (from 09:00 to 22:00, with a step of 30 minutes) (only enable to pick available times for the specific court)
         # if reservation is for Saturday or Sunday, possibletimes are until 20:00 only (use possibletimesweekend array for choices)
         # validation with date and time restrictions included (reservations need to be made a minimum of 30 minutes before playing time)
+        # reservations can only be made for the current and the next month
     # database
         # define schema (consider admin type)
         # create tables
---
+++ validation.py
@@ -0,0 +1,60 @@
+from flask import Flask, flash, redirect, render_template, request
+from datetime import datetime
+
+
+def validate(name, people, court, date, time, numofpeople, courts, ptw, pt):
+    if name == "" or people == "" or court == "" or date == "" or time == "Choose a time":
+            flash("Please fill out all fields")
+            return False
+
+    selected_date_str = date.strftime("%Y-%m-%d") # string selected date 
+    current_time_str = datetime.now().strftime("%H:%M") # string
+    selected_weekday = date.strftime("%a") # string current day of the week abbreviation (3 characters)
+
+    # selected date/time data in int
+    selected_hour = int(time[:2])
+    selected_minute = int(time[3:])
+    selected_year = int(selected_date_str[:4])
+    selected_month = int(selected_date_str[5:7])
+    selected_day = int(selected_date_str[8:])
+
+    # current date/time data in int
+    current_hour = int(current_time_str[:2]) # int
+    current_minute = int(current_time_str[3:]) # int
+    current_year = datetime.now().year # int
+    current_month = datetime.now().month # int
+    current_day = datetime.now().day # int
+
+    # Own validation
+    if people not in numofpeople:
+        flash("Only 2 to 4 people are allowed per court")
+        return False
+    elif court not in courts:
+        flash("Please select a valid court")
+        return False
+    elif ((selected_weekday == "Sat" or selected_weekday == "Sun") and time not in ptw) or time not in pt:
+        flash("The club is not open at this time. Week hours are 09:00 to 22:00. Weekend hours are 09:00 to 20:00 (reservations are allowed until 1 hour before closing)")
+        return False
+    elif selected_year < current_year or (selected_year == current_year and (selected_month < current_month) or (selected_month == current_month and selected_day < current_day)):
+        flash("Please provide a valid date")
+        return False
+    elif selected_year == current_year and selected_month == current_month and selected_day == current_day and selected_hour <= current_hour + 1:
+        if selected_hour == current_hour + 1 and (60 + selected_minute - current_minute) <= 30:
+            flash("Online reservations can only be made at least 30 minutes prior to palying time")
+            return False
+        elif selected_hour == current_hour + 1 and (60 + selected_minute - current_minute) > 30:
+            return True #######################
+        else:
+            flash("Please select a valid time")
+            return False
+    elif selected_month > current_month + 1:
+        flash("Reservations are only allowed for the current and the next month")
+        return False
+    elif selected_year > current_year:
+        if current_month == 12 and selected_month == 1:
+            return True ###############################
+        else:
+            flash("Reservations are only allowed for the current and the next month")
+            return False
+    else:
+        return True #################################
