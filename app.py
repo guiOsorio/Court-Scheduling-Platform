a40d5213913729efe2198254dbc011e8f65d579b
guiOsorio <gosorio@sandiego.edu>
2021-07-28 20:13:06 +0100

    makeIndex completed

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import generate_password_hash
 
-from helpers import login_required, validateBooking, validateLogin, validateRegistration
+from helpers import login_required, validateBooking, validateLogin, validateRegistration, validateIndex, makeIndex
 
 # Initialize app
 app = Flask(__name__)
@@ -62,8 +62,13 @@ class BookingForm(FlaskForm):
     date = DateField('Date', format='%Y-%m-%d', default=datetime.now())
     time = SelectField('Time (open from 09:00 to 22:00)', choices=possibletimes, default="Choose a time")
 
-# Adming check bookings form
+# Admin check bookings form
 class CheckBookingsForm(FlaskForm):
+    date = DateField('Date', format='%Y-%m-%d', default=datetime.now())
+
+# Admin book a court for a day form
+class BookDayForm(FlaskForm):
+    court = SelectField('Court', choices=courts)
     date = DateField('Date', format='%Y-%m-%d', default=datetime.now())
 
 
@@ -97,14 +102,14 @@ def booking():
             con = sqlite3.connect("scheduling.db")
             cur = con.cursor()
 
-            selected_weekday = date.strftime("%A") # string current day of the week abbreviation (3 characters)
+            selected_weekday = date.strftime("%A") # string current day of the week
 
             cur.execute("""INSERT INTO bookings (user_id, week_day, date, time, court, people) 
                         VALUES (:user_id, :week_day, :date, :time, :court, :people)""", 
                         {"user_id": user_id, "week_day": selected_weekday, "date": selected_date, "time": time, "court": court, "people": people})
             con.commit()
 
-            flash("Booking successful!!")
+            flash("Booking successful!!", "success")
 
             return redirect("/")
 
@@ -136,7 +141,7 @@ def login():
         id = cur.fetchone()[0]
 
         # Greet user
-        flash(f"Welcome, {username} !!")
+        flash(f"Welcome, {username} !!", "success")
 
         # Remember which user has logged in
         session["user_id"] = id
@@ -171,7 +176,7 @@ def register():
             hashed_password = generate_password_hash(password) # hash password
             cur.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)", {"username": username, "hash": hashed_password})
             con.commit()
-            flash("Account successfully created!")
+            flash("Account successfully created!", "success")
 
             # Get id of user who logged in to store in the session
             cur.execute("SELECT * FROM users WHERE username = :username", {"username": username})
@@ -192,6 +197,8 @@ def logout():
     # Forget any user_id
     session.clear()
 
+    flash("Goodbye", "info")
+
     # Redirect user to login form
     return redirect("/login")
 
@@ -209,7 +216,7 @@ def upcoming():
         id = request.form.get("id")
         cur.execute("DELETE FROM bookings WHERE booking_id = :id", {"id": id})
         con.commit()
-        flash("Successfully deleted!!")
+        flash("Successfully deleted!!", "success")
 
     # Find upcoming bookings for logged in user
     cur.execute(""" SELECT week_day, date, time, court, people, booking_id FROM bookings
@@ -226,14 +233,15 @@ def upcoming():
 @app.route("/admin", methods=["GET", "POST"])
 def admin():
 
-    # Initialize form
-    form = CheckBookingsForm()
+    # Initialize forms
+    form_check = CheckBookingsForm()
+    form_bookday = BookDayForm()
 
     user_id = session["user_id"] # store current user's id
     
     # POST request for searching bookings for a certain day (default for today)
-    if form.validate_on_submit():
-        date = form.date.data # datetime.date
+    if form_check.validate_on_submit():
+        date = form_check.date.data # datetime.date
         selected_date = date.strftime("%Y-%m-%d") # string
 
         # Connect to database
@@ -247,11 +255,11 @@ def admin():
 
         if not bookings_data:
             msg = "No bookings for that day"
-            return render_template("admin.html", form=form, msg=msg)
+            return render_template("admin.html", form_check=form_check, msg=msg, form_bookday=form_bookday)
         else:
-            return render_template("admin.html", form=form, bd=bookings_data)
+            return render_template("admin.html", form_check=form_check, bd=bookings_data, form_bookday=form_bookday)
 
-    return render_template("admin.html", form=form)
+    return render_template("admin.html", form_check=form_check, form_bookday=form_bookday)
 
 
 @app.route("/delete", methods=["POST"])
@@ -266,7 +274,7 @@ def delete():
     # Delete booking
     cur.execute("DELETE FROM bookings WHERE booking_id = :id", {"id": id})
     con.commit()
-    flash("Succesfully deleted")
+    flash("Succesfully deleted", "success")
 
     return redirect("/admin")
 
@@ -274,22 +282,49 @@ def delete():
 @app.route("/createindex", methods=["POST"])
 def createindex():
 
-    # Connect to database
-    con = sqlite3.connect("scheduling.db")
-    cur = con.cursor()
-
     name = request.form.get("name")
     table = request.form.get("table")
-    columns = request.form.get("columns") # transform into list, then depending on its size, pass each column to the query
+    columns_input = request.form.get("columns")
 
-    # FORM VALIDATION
+    # transform columns into a list
+    columns = columns_input.split(",")
+    # remove whitespaces from each element of the list
+    i = 0
+    for column in columns:
+        columns[i] = column.strip()
+        i += 1
 
-    # CREATE INDEX
+    # turn invalid SQL character into _
+    k = 0
+    for n in name:
+        if n == " " or n == ";" or n == ">" or n == "<" or n == ">":
+            name[k] = "_"
+        k += 1
 
-# delete route (only accepts POST) - delete a booking
-# makeindex route (only accepts POST) - create an index CREATE UNIQUE INDEX unique_com_id ON company (com_id,com_name);
-# daybooking route (only accepts POST) - deletes bookings for the selected day (if any exists) and disables bookings for that day
+    if not validateIndex(name, table, columns_input, columns):
+        return redirect("/admin")
+    
+    makeIndex(columns, name, table)
 
+    return redirect("/admin")
+
+
+@app.route("/bookday", methods=["POST"])
+def bookday():
+    # bookday route (only accepts POST) - admin has the ability to delete bookings for the selected day (if any exists) and disable bookings for that day
+
+    user_id = session["user_id"] # store current user's id, pas this into query
+    court = request.form.get("court") # pass this into query
+    date = request.form.get("date")
+    selected_date = date.strftime("%Y-%m-%d") # pass this into query
+    selected_weekday = date.strftime("%A") # pass this into query
+    people = 4
+    
+    # delete every booking for the selected_date and court
+    # use a for loop to make a booking for every possible time in the day for the selected court 
+    # (use array possibletimes for week days and possibletimesweekend if the selected_weekday is a weekend day)
+
+    return redirect("/admin")
 
 
 # APP REQUIREMENTS
@@ -308,16 +343,17 @@ def createindex():
         # have action for user to cancel booking, which makes the times of it available again
         # create indexes where useful
     # admin 
+        # show all bookings for each court for the selected day (default for today)
+        # delete route (only accepts POST) - admin has the ability to delete any reservation for any day and to book a court for an extended period of time (more than 1 hour per day)
+        # createindex route (only accepts POST) - admin is able to create indexes on tables
+        # bookday route (only accepts POST) - admin has the ability to delete bookings for the selected day (if any exists) and disable bookings for that day
         # have way to register an admin with a special login
         # admin logs in as a regular user
-        # create function on helpers.py to restrict admin page to be accesible by admin only (similar to login_required
-        # admin is able to create indexes on tables
-        # show all bookings for each court for the selected day (default for today)
-        # admin has the ability to delete any reservation for any day and to book a court for an extended period of time (more than 1 hour)
-        # admin has the ability to close club for a specified day
+        # create function on helpers.py to restrict admin page to be accesible by admin only (similar to login_required)
     # more
         # send me an email when an appointment or cancellation is made, with the action's info
         # if there are appointments in the next hour, send me an email with all the appointments
         # schedule Python code to run every 6 months to remove bookings from database and other features if needed
         # page which shows all current bookings for the logged in account
+        # basic styling
 
--- helpers.py
+++ helpers.py
@@ -15,7 +15,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
-            flash("Login required")
+            flash("Login required", "danger")
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
@@ -23,7 +23,7 @@ def login_required(f):
 
 def validateBooking(people, court, date, time, numofpeople, courts, ptw, pt, user_id):
     if people == "" or court == "" or date == "" or time == "Choose a time":
-            flash("Please fill out all fields")
+            flash("Please fill out all fields", "danger")
             return False
 
     selected_date = date.strftime("%Y-%m-%d") # string
@@ -60,40 +60,40 @@ def validateBooking(people, court, date, time, numofpeople, courts, ptw, pt, use
 
     # Own validation
     if people not in numofpeople:
-        flash("Only 2 to 4 people are allowed per court")
+        flash("Only 2 to 4 people are allowed per court", "danger")
         return False
     elif court not in courts:
-        flash("Please select a valid court")
+        flash("Please select a valid court", "danger")
         return False
     elif ((selected_weekday == "Sat" or selected_weekday == "Sun") and time not in ptw) or time not in pt:
-        flash("The club is not open at this time. Week hours are 09:00 to 22:00. Weekend hours are 09:00 to 20:00 (bookings are allowed until 1 hour before closing)")
+        flash("The club is not open at this time. Week hours are 09:00 to 22:00. Weekend hours are 09:00 to 20:00 (bookings are allowed until 1 hour before closing)", "danger")
         return False
     elif selected_year < current_year or (selected_year == current_year and (selected_month < current_month) or (selected_month == current_month and selected_day < current_day)):
-        flash("Please provide a valid date")
+        flash("Please provide a valid date", "danger")
         return False
     elif selected_day_bookings[0] >= 2 and selected_day_bookings[1] != "admin":
-        flash("A maximum of 2 bookings per person per day is allowed")
+        flash("A maximum of 2 bookings per person per day is allowed", "danger")
         return False
     elif is_booked:
-        flash("This court is already booked at this time. Please select a different court or time")
+        flash("This court is already booked at this time. Please select a different court or time", "danger")
         return False
     elif selected_year == current_year and selected_month == current_month and selected_day == current_day and selected_hour <= current_hour + 1:
         if selected_hour == current_hour + 1 and (60 + selected_minute - current_minute) <= 30:
-            flash("Online bookings can only be made at least 30 minutes prior to playing time")
+            flash("Online bookings can only be made at least 30 minutes prior to playing time", "danger")
             return False
         elif selected_hour == current_hour + 1 and (60 + selected_minute - current_minute) > 30:
             return True #######################
         else:
-            flash("Please select a valid time")
+            flash("Please select a valid time", "danger")
             return False
     elif selected_month > current_month + 1:
-        flash("Bookings are only allowed for the current and the next month")
+        flash("Bookings are only allowed for the current and the next month", "danger")
         return False
     elif selected_year > current_year:
         if current_month == 12 and selected_month == 1:
             return True ###############################
         else:
-            flash("Bookings are only allowed for the current and the next month")
+            flash("Bookings are only allowed for the current and the next month", "danger")
             return False
     else:
         return True #################################
@@ -102,12 +102,12 @@ def validateBooking(people, court, date, time, numofpeople, courts, ptw, pt, use
 def validateLogin(username, password):
     # Ensure username was submitted
     if not username:
-        flash("Must have a username")
+        flash("Must have a username", "danger")
         return False
 
     # Ensure password was submitted
     elif not password:
-        flash("Must have a password")
+        flash("Must have a password", "danger")
         return False
 
     # Connect to database
@@ -118,13 +118,13 @@ def validateLogin(username, password):
     user = cur.fetchone()
 
     if not user:
-        flash("Username doesn't exist")
+        flash("Username doesn't exist", "danger")
         return False
 
     # Ensure username exists and password is correct
     table_hash = user[2]
     if not check_password_hash(table_hash, password):
-        flash("Invalid password")
+        flash("Invalid password", "danger")
         return False
 
     return True
@@ -134,17 +134,83 @@ def validateRegistration(usernames, username, password, confirmation):
     # Handles username already existing
         for user in usernames:
             if username in user:
-                flash("This username is already taken")
+                flash("This username is already taken", "danger")
                 return False
 
         # Handles username or password fields being blank
         if username.strip() == "" or password.strip() == "" or confirmation.strip() == "":
-            flash("All items need a value")
+            flash("All items need a value", "danger")
             return False
         
         # Handles password and password confirmation not matching
         elif password != confirmation:
-            flash("Password and confirmation fields don't match")
+            flash("Password and confirmation fields don't match", "danger")
             return False
         
         return True
+
+
+def validateIndex(name, table, columns_input, columns):
+    # CREATE INDEX VALIDATION
+
+    # Connect to database
+    con = sqlite3.connect("scheduling.db")
+    cur = con.cursor()
+
+    if name == "" or table.strip() == "" or columns_input.strip() == "":
+        flash("All fields need a value", "danger")
+        return False
+
+    # if table doesn't exist - table needs to exist and have at least one row to have an index created
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    tables_in_db = cur.fetchall()
+
+    # check if table exists
+    num_of_tables = len(tables_in_db)
+    z = 0
+    
+    for t in tables_in_db:
+        if t[0] == table:
+            break
+        elif z == num_of_tables - 1: # no corresponding table found in db
+            flash("Table does not exist", "danger")
+            return False
+        else:
+            z += 1
+    
+    # if any of the columns do not belong to the table - for loop for every column field
+    for column in columns:
+        cur.execute("SELECT 1 FROM PRAGMA_TABLE_INFO(:table) WHERE name = :column", {"table": table, "column": column})
+        if cur.fetchone() is None:
+            flash("Column does not exist", "danger")
+            return False
+
+    return True
+
+
+def makeIndex(columns, name, table):
+    # CREATE INDEX
+
+    # Connect to database
+    con = sqlite3.connect("scheduling.db")
+    cur = con.cursor()
+
+    num_of_columns = len(columns)
+    if num_of_columns == 1:
+        cur.execute("CREATE UNIQUE INDEX "+name+" ON "+table+" ("+columns[0]+")")
+    elif num_of_columns == 2:
+        cur.execute("CREATE UNIQUE INDEX "+name+" ON "+table+" ("+columns[0]+", "+columns[1]+")")
+    elif num_of_columns == 3:
+        cur.execute("CREATE UNIQUE INDEX "+name+" ON "+table+" ("+columns[0]+", "+columns[1]+", "+columns[2]+")")
+    elif num_of_columns == 4:
+        cur.execute("CREATE UNIQUE INDEX "+name+" ON "+table+" ("+columns[0]+", "+columns[1]+", "+columns[2]+", "+columns[3]+")")
+    elif num_of_columns == 5:
+        cur.execute("CREATE UNIQUE INDEX "+name+" ON "+table+" ("+columns[0]+", "+columns[1]+", "+columns[2]+", "+columns[3]+", "+columns[4]+")")
+    else:
+        flash("Too many columns", "danger")
+        return
+
+    con.commit()
+    flash("Index created successfully", "success")
+
+    return
