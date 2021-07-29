5d2ade62bb79b0d0d0988752c7858c57f000df86
guiOsorio <gosorio@sandiego.edu>
2021-07-29 16:00:00 +0100

    admin able to search bookings by court

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ Session(app)
 # Arrays for SelectField choices
 numofpeople = ["2", "3", "4"]
 courts = ["1", "2", "3"]
+courts_all = ["1", "2", "3", "All courts"]
 possibletimes = ["Choose a time",
     "09:00", "09:30",
     "10:00", "10:30",
@@ -67,8 +68,8 @@ class CheckBookingsForm(FlaskForm):
     date = DateField('Date', format='%Y-%m-%d', default=datetime.now())
 
 # Admin book a court for a day form
-class BookDayForm(FlaskForm):
-    court = SelectField('Court', choices=courts)
+class CourtDayForm(FlaskForm):
+    court = SelectField('Court', choices=courts_all)
     date = DateField('Date', format='%Y-%m-%d', default=datetime.now())
 
 
@@ -195,13 +196,8 @@ def register():
 def logout():
     """Log user out"""
 
-    user_id = session["user_id"] # store current user's id
-
-
     # Forget any user_id
     session.clear()
-
-    flash("Goodbye", "info")
 
     # Redirect user to login form
     return redirect("/login")
@@ -219,6 +215,19 @@ def mybookings():
     # Connect to database
     con = sqlite3.connect("scheduling.db")
     cur = con.cursor()
+
+    # current_date in same format as added to db (string and %Y-%m-%d)
+    current_date = datetime.now()
+    current_date_str = current_date.strftime("%Y-%m-%d")
+
+    # get number of upcoming bookings for the current user
+    cur.execute("SELECT COUNT(*) FROM bookings WHERE user_id = :user_id AND date >= :current_date", {"user_id": user_id, "current_date": current_date_str})
+    upcoming_user_bookings = cur.fetchone()[0]
+
+    # check if there are any upcoming bookings, if not, return no upcoming bookings for {user}
+    if upcoming_user_bookings == 0:
+        msg = "You have no upcoming bookings"
+        return render_template("mybookings.html", form=form, msg=msg)
 
     if "check_day" in request.form and form.validate_on_submit():
 
@@ -238,16 +247,12 @@ def mybookings():
 
     if "show_all" in request.form:
         
-        # Find bookings for logged in user
+        # Find bookings for logged in user and add them to page
         cur.execute(""" SELECT week_day, date, time, court, people, booking_id FROM bookings
-                    WHERE user_id = :user_id ORDER BY date, time""", {"user_id": user_id})
+                    WHERE user_id = :user_id AND date >= :current_date ORDER BY date, time""", {"user_id": user_id, "current_date": current_date_str})
         bookings_data = cur.fetchall()
 
-        if not bookings_data:
-            msg = "No bookings for this account"
-            return render_template("mybookings.html", form=form, msg=msg)
-        else:
-            return render_template("mybookings.html", form=form, bd=bookings_data)
+        return render_template("mybookings.html", form=form, bd=bookings_data)
     
     if "delete_day" in request.form and form.validate_on_submit:
 
@@ -281,13 +286,14 @@ def mybookings():
 def admin():
 
     # Initialize forms
-    form_check = CheckBookingsForm()
-    form_bookday = BookDayForm()
+    form_check = CourtDayForm()
+    form_bookday = CourtDayForm()
 
     user_id = session["user_id"] # store current user's id
     
     # POST request for searching bookings for a certain day (default for today)
     if form_check.validate_on_submit():
+        court = form_check.court.data
         date = form_check.date.data # datetime.date
         selected_date = date.strftime("%Y-%m-%d") # string
 
@@ -295,14 +301,21 @@ def admin():
         con = sqlite3.connect("scheduling.db")
         cur = con.cursor()
 
-        # Find upcoming bookings for that day
-        cur.execute(""" SELECT week_day, date, time, court, people, booking_id FROM bookings
-                    WHERE date = :date ORDER BY time """, {"date": selected_date})
-        bookings_data = cur.fetchall()
-
+        # Find upcoming bookings for that day if all courts option is provided
+        if court == "All courts":
+            cur.execute(""" SELECT week_day, date, time, court, people, booking_id FROM bookings
+                        WHERE date = :date ORDER BY time """, {"date": selected_date})
+        else:
+            cur.execute(""" SELECT week_day, date, time, court, people, booking_id FROM bookings
+                        WHERE date = :date AND court = :court ORDER BY time """, {"date": selected_date, "court": court})
+        bookings_data = cur.fetchall() # populate data to display
         if not bookings_data:
-            msg = "No bookings for that day"
-            return render_template("admin.html", form_check=form_check, msg=msg, form_bookday=form_bookday)
+            if court == "All courts":
+                msg = "No bookings for the selected day"
+                return render_template("admin.html", form_check=form_check, msg=msg, form_bookday=form_bookday)
+            else:
+                msg = "No bookings for this court on the selected day"
+                return render_template("admin.html", form_check=form_check, msg=msg, form_bookday=form_bookday)
         else:
             return render_template("admin.html", form_check=form_check, bd=bookings_data, form_bookday=form_bookday)
 
@@ -371,7 +384,7 @@ def createindex():
 def bookday():
     # bookday route (only accepts POST) - admin has the ability to delete bookings for the selected day (if any exists) and disable bookings for that day
 
-    form_bookday = BookDayForm()
+    form_bookday = CourtDayForm()
 
     user_id = session["user_id"] # store current user's id, pass this into query
     court = form_bookday.court.data # pass this into query
@@ -417,63 +430,14 @@ def bookday():
 
     return redirect("/admin")
 
-# on mybookings page and admin page, only show upcoming bookings
-# have admin searches be able to be by court (have "all courts" option to show results for all courts)
+
+# NEXT TODOS
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# change admin forms to submit to /admin and figure it out by input submit id
 # have admin be able to search how many bookings there are for a specific day and show them and how many upcoming bookings there are in total and total bookings (past plus upcoming)
 # session not ending properly when I restart flask
 # admin registering
+# store admin in session as a boolean isAdmin if type is admin when logging in
 # admin_required
 # create helpers folder with files such as validations.py (to store validation functions), required.py (to store required functions like login_required), and more if necessary
-
-
-# APP REQUIREMENTS
-    # version 1.0 - original implementation for Luis
-        # form 
-            # displays a calendar daypicker but with invalid dates disabled (already past dates, days where club is closed, days more than a month from today)
-            # displays only options of valid times (from 09:00 to 22:00, with a step of 30 minutes) (only enable to pick available times for the specific court)
-            # if reservation is for Saturday or Sunday, possibletimes are until 20:00 only (use possibletimesweekend array for choices)
-            # validation with date and time restrictions included (bookings need to be made a minimum of 30 minutes before playing time)
-            # bookings can only be made for the current and the next month
-        # database
-            # define schema (consider admin type)
-            # create tables
-            # insert SQL statements in app.py, where needed
-            # maximum of 2 bookings per day per person (1 hour) (except for admin)
-            # a booking is for 30 minutes (after booking is done, make the time for that specific day and court unavailable)
-            # have action for user to cancel booking, which makes the times of it available again
-            # create indexes where useful
-        # admin 
-            # show all bookings for each court for the selected day (default for today)
-            # delete route (only accepts POST) - admin has the ability to delete any reservation for any day and to book a court for an extended period of time (more than 1 hour per day)
-            # createindex route (only accepts POST) - admin is able to create indexes on tables
-            # bookday route (only accepts POST) - admin has the ability to delete bookings for the selected day (if any exists) and disable bookings for that day
-            # have way to register an admin with a special login
-            # admin logs in as a regular user
-            # create function on helpers.py to restrict admin page to be accesible by admin only (similar to login_required)
-        # more
-            # send me an email when an booking or cancellation is made, with the action's info
-            # send email to user after a booking is made
-            # send email to user if one of its bookings are cancelled (both by the user himself or an admin)
-            # if there are bookings in the next hour, send me an email with all the bookings
-            # if a user has a booking in the next hour, send him an email
-            # schedule Python code to run every 6 months to remove bookings from database and other features if needed
-            # page which shows all current bookings for the logged in account
-            # basic styling
-            # organize code
-            # deploy and show to Luis
-    # version 2.0 - goal is to be a platform where users can book a court for different clubs
-        # change bookings to be of 1 hour instead of 30 minutes, while still allowing appointments to start at hour:00 and hour:30 (don't forget to update booking validation)
-        # create new table to store different clubs (have club_name, number of courts, opening times, exact location, address)
-        # update schema
-        # have map (use mapbox) showing location of all courts
-        # create a profile page for each tennis club
-        # when booking, have option to select club (only show affiliated clubs). After selecting club, go to a new page where a booking for that club can be made
-        # update queries to show results for club when relevant
-        # update queries to show the club where the booking is made
-        # update insert and delete queries to only delete bookings for specified club
-        # have type super-admin (has the ability to do be an admin for every club in every sport)
-        # admin type only allows to perform actions on the admin's specific club
-        # contacts page
-    # version 3.0 - have booking tennis courts be one of many possible types of bookings to be made, implement booking for other sports such as soccer, basketball, etc
-    # version 4.0 - implement non-sport related bookings, such as for restaurant tables, offices (like lawyer, doctor, etc), hair salons, etc
-
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
